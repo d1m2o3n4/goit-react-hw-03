3dcd640ba3a15a992bb6f58ebb8d16e3fd52df1b
d1m2o3n4 <d1m2o3n4@gmail.com>
2024-05-05 15:29:38 +0300

    create

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import ContactList from "./components/contactList/ContactList";
+import SearchBox from "./components/searchBox/SearchBox";
+import ContactForm from "./components/contactForm/ContactForm";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getStoredContacts = () => {
+    const storedContacts = localStorage.getItem("contacts");
+    console.dir(storedContacts);
+    return storedContacts.length > 2
+      ? JSON.parse(storedContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  };
+  // return storedContacts
+  //   ? JSON.parse(storedContacts)
+  // : [
+  //     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  //     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  //     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  //     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  //   ];
+
+  // const initialContacts = [
+  //   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  //   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  //   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  //   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  // ];
+  const [searchTerm, setSearchTerm] = useState("");
+  const [contacts, setContacts] = useState(getStoredContacts);
+  console.log(contacts);
+  const addContact = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    setContacts([...contacts, newContact]);
+  };
+  const handleDelete = (contId) => {
+    setContacts(
+      contacts.filter((item) => {
+        return item.id !== contId;
+      })
+    );
+  };
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="formWrapper">
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <SearchBox handleChange={handleChange} />
+      <ContactList contacts={filteredContacts} handleDelete={handleDelete} />
+    </div>
+  );
 }
 
-export default App
+export default App;
